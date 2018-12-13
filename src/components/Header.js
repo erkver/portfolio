1416d62861a8ef8fb2edf2fd1a1547cc6d7f6f0d
Eric Vere <erkver250@gmail.com>
2018-12-13 17:27:10 -0600

    Added dropdown menu to mobile and tablet views

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,51 +4,85 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.png'
 import logo from '../../public/icons/website-icon.png'
 
-const hamburgerStyles = {width: "25px", height: "25px"};
-
 class Header extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      visible: false
+      visible: false,
     }
   }
-    render() {
-      const { visible } = this.state;
-      return (
-        <header id="header">
-          <div className="navbar">
+
+  handleClick = () => {
+    this.setState({ visible: true }, () => {
+      document.addEventListener('click', this.handleOutsideClick)
+    })
+  }
+
+  handleOutsideClick = () => {
+    this.setState({ visible: false }, () => {
+      document.removeEventListener('click', this.handleOutsideClick)
+    })
+  }
+
+  render() {
+    const { visible } = this.state
+    return (
+      <header id="header">
+        <div className="navbar">
           <div className="nav-content">
             <button
-              // styles={hamburgerStyles}
-              className={`hamburger hamburger--collapse ${!visible ? "" : "is-active"}`}
-              onClick={() => this.setState({visible: !visible})}
+              className={`hamburger hamburger--collapse ${
+                !visible ? '' : 'is-active'
+              }`}
+              onClick={this.handleClick}
             >
-              <span style={{"marginTop": "10px"}} className="hamburger-box">
-                <span className="hamburger-inner"></span>
+              <span style={{ marginTop: '10px' }} className="hamburger-box">
+                <span className="hamburger-inner" />
               </span>
             </button>
             <div className="logo-tt">
               <p className="title">Eric Vere</p>
               <img className="my-logo" src={logo} alt="" />
             </div>
-            </div>
           </div>
-          <div className="inner">
-            <img className="image avatar" src={avatar} alt="" />
-            <h1>
-              <strong>I am Eric Vere</strong>,<br /> developer, cat-dad, and music junkie.
-            </h1>
-            <div className="link-cont">
-              <h3 onClick={() => document.getElementById("one").scrollIntoView(true)}>About Me</h3>
-              <h3 onClick={() => document.getElementById("two").scrollIntoView(true)}>Projects</h3>
-              <h3 onClick={() => document.getElementById("three").scrollIntoView(true)}>Contact Me</h3>
-            </div>
+        </div>
+        <div className="inner">
+          <img className="image avatar" src={avatar} alt="" />
+          <h1>
+            <strong>I am Eric Vere</strong>,<br /> developer, cat-dad, and music
+            enthusiast.
+          </h1>
+          <div className={`link-cont ${visible}`}>
+            <h3
+              onClick={() => {
+                document.getElementById('main').scrollIntoView(true)
+                this.setState({ visible: false })
+              }}
+            >
+              About Me
+            </h3>
+            <h3
+              onClick={() => {
+                document.getElementById('two').scrollIntoView(true)
+                this.setState({ visible: false })
+              }}
+            >
+              Projects
+            </h3>
+            <h3
+              onClick={() => {
+                document.getElementById('three').scrollIntoView(true)
+                this.setState({ visible: false })
+              }}
+            >
+              Contact Me
+            </h3>
           </div>
-          <Footer />
-        </header>
-      )
-    }
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
